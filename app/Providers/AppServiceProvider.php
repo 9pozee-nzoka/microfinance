3029cb704f7b4bca8c51d10bbb6b1af3a35f3221
dex <dex@DEVELOPER.KSG.AC.KE>
2026-05-26 08:33:42 -0400

    Force HTTPS in production to fix insecure form warning

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    
     public function boot(): void
     {
-        //
+        // Force HTTPS in production (fixes "form not secure" browser warning)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
     }
+}
 }
